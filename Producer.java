5e53ad0b7aabca93758b6efacfbe0cbe51057f3b
Bryant Vaughn <bf_vaughn@hotmail.com>
2019-04-14 19:37:12 -0600

    adding producer.java file

---
+++ Producer.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.Semaphore;
+
+public class Producer implements Runnable {
+	Q q;
+	Producer(Q q) {
+		this.q = q;
+		new Thread(this, "Producer").start();
+	}
+
+	public void run() {
+		for(int i = 0; i < 5; i++) {
+			// producer put items
+			q.put(i);
+		}
+	}
+}
