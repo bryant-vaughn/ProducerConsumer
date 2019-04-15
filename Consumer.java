eecf381f7cb6ab1fd4edd1f7e1de8be765704034
Bryant Vaughn <bf_vaughn@hotmail.com>
2019-04-14 19:40:23 -0600

    adding Consumer.java file

---
+++ Consumer.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.Semaphore;
+
+public class Consumer implements Runnable {
+	Q q;
+	Consumer(Q q) {
+		this.q = q;
+		new Thread(this, "Consumer").start();
+	}
+
+	public void run() {
+		for(int i = 0; i < 5; i++) {
+			// consumer get items
+			q.get();
+		}
+	}
+}
